800e441ee5415698e41e20cd5580e8eadae76aaa
Julia McGuigan <julia.mcg4n@gmail.com>
2021-09-16 16:22:32 -0400

    Explained how we will need to filter assets. Working filter.

--- AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
+++ AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
@@ -8,22 +8,33 @@ namespace AvaloniaApplication1.ViewModels {
     using System.Diagnostics;
 
     public class MainWindowViewModel : ViewModelBase {
-        public List<AssetInfo> assets_;
+        /// <summary>
+        /// The view model info of the available assets in the grid. 
+        /// </summary>
         public RangeEnabledObservableCollection<AssetInfo> Assets { get; set; }
+        // TRACKS THE VIEW DATA
+        // WE WILL NEED TO ADD A SEPARATE PROPERTY TO TRACK THE DATA ENTRIES
+        // USE THE DATA LAYER TO CONFIGURATE THE DISPLAYED DATA IN THE VIEW MODEL 
 
         public MainWindowViewModel() {
-            assets_ = GenerateData().ToList();
-            Assets = new RangeEnabledObservableCollection<AssetInfo>(assets_);
+            Assets = new RangeEnabledObservableCollection<AssetInfo>(GenerateData().ToList());
         }
 
         public void OnClickCommand() {
             var sw = Stopwatch.StartNew();
-            foreach (var item in assets_) {
-                if (!item.AssetName.Contains("BIG")) {
-                    Assets.Remove(item);
-                    assets_.Remove(item);
-                }
-            }
+            // when iterating over a collection, do not change items in the collection!
+            //foreach (var item in assets_) {
+            //    if (!item.AssetName.Contains("BIG")) {
+            //        Assets.Remove(item);
+            //        assets_.Remove(item);
+            //    }
+            //}
+            // you can do a temporary variable in the iterator instead, but this is no less efficient in this scenario 
+            // if it gets weird you may want to troubleshoot LINQ vs. a manually optmized query; but that usually applies to a data source like SQL 
+            // where it can generate weird queries
+            var filteredAssets = Assets.Where(i => !string.IsNullOrEmpty(i.AssetName) && i.AssetName.Contains("BIG")).ToList();
+            Assets.ResetToRange(filteredAssets);
+
             sw.Stop();
             Console.WriteLine("OnClickCommand took ms:" + sw.ElapsedMilliseconds);
         }
